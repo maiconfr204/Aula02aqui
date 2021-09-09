a453bbcb85d76c2679d651e9d2e1bb34e66a1533
Guilherme Terenciani <guilhereterenciani@gmail.com>
2021-09-09 15:11:10 -0400

    finalização do código de navegação entre as telas do projeto

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Text, View} from 'react-native';
 
-import Home from './src/Pages/Home';
+import Routes from './src/routes';
 
 
 
 export default function App() {
     return (  
-        <View>
-            <Home/>
+        <View style={{flex:1}}>
+            <Routes/>
         </View>
     );
 }
